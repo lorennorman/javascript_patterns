1a6c76a0215a7b5cb29cf0e4c7033a064ed0a7a5
Loren Norman <lorennorman@gmail.com>
2010-08-05 00:45:11 -0400

    Added support for monkey-patching and -unpatching. Extended with a safe model for adding and removing entire sets of patches, useful for creating DSL.

--- spec/MonkeyPatchSpec.js
+++ spec/MonkeyPatchSpec.js
@@ -56,4 +56,82 @@ describe('Scoped Monkey-patching', function()
       });
     });
   });
+
+  describe('DSL creation', function()
+  {
+    it('should be a factory for patch-sets', function()
+    {
+      var myMonkeyPatch = Monkey.create();
+      expect(myMonkeyPatch).toBeDefined();
+    });
+
+    describe('using a DSL', function()
+    {
+      var countingDSL;
+      var timesFunction = function(iterateFunction)
+      {
+        for(var i = 0; i < Number(this); i++) { iterateFunction(i); }
+      }
+      beforeEach(function()
+      {
+        countingDSL = Monkey.create();
+      });
+
+      it('should allow registration of patches', function()
+      {
+        expect(countingDSL.registerPatch).toBeAFunction();
+        countingDSL.registerPatch(Number, 'times', timesFunction);
+        expect((50).times).not.toBeDefined(); // Registering does not apply it yet
+      });
+
+      it('should allow activation of patches', function()
+      {
+        countingDSL.registerPatch(Number, 'times', timesFunction);
+
+        expect(countingDSL.activate).toBeAFunction();
+        countingDSL.activate();
+        expect((50).times).toBeDefined();
+
+        countingDSL.deactivate();
+      });
+
+      it('should allow deactivation of patches', function()
+      {
+        countingDSL.registerPatch(Number, 'times', timesFunction);
+        countingDSL.activate();
+
+        expect(countingDSL.deactivate).toBeAFunction();
+        countingDSL.deactivate();
+        expect((50).times).not.toBeDefined();
+      });
+
+      it('should allow wrapping of functions where the patch is active', function()
+      {
+        countingDSL.registerPatch(Number, 'times', timesFunction);
+
+        expect(countingDSL.wrap).toBeAFunction();
+
+        var counter = 0;
+        var incrementFunc = function() { counter++; };
+
+        countingDSL.wrap(function()  // Inside this function our DSL is active
+        {
+          (10).times(incrementFunc); // Call the increment function 10 times
+        });
+
+        expect(counter).toEqual(10); // Ensure 10 increments happened
+        expect((10).times).not.toBeDefined(); // Ensure the patch was removed
+      });
+    });
+  });
 });
+
+
+
+
+
+
+
+
+
+
--- src/monkey_patch.js
+++ src/monkey_patch.js
@@ -1,6 +1,7 @@
 // Namespace
 var Monkey = {};
 
+// Naive, global patching functions. Dangerous to use directly.
 Monkey.patch = function(targetClass, functionName, functionObject)
 {
   targetClass.prototype[functionName] = functionObject;
@@ -9,4 +10,49 @@ Monkey.patch = function(targetClass, functionName, functionObject)
 Monkey.unpatch = function(targetClass, functionName)
 {
   delete targetClass.prototype[functionName];
+};
+
+// A factory for objects that manage individual patch-sets
+Monkey.create = function()
+{
+  var patchSet = [];
+
+  var _registerPatch = function(klassToPatch, funcName, funcImp)
+  {
+    patchSet.push(
+    { klass:        klassToPatch
+    , functionName: funcName
+    , functionImp:  funcImp
+    });
+  };
+
+  var _activate = function()
+  {
+    patchSet.forEach(function(patch)
+    {
+      Monkey.patch(patch.klass, patch.functionName, patch.functionImp);
+    });
+  };
+
+  var _deactivate = function()
+  {
+    patchSet.forEach(function(patch)
+    {
+      Monkey.unpatch(patch.klass, patch.functionName);
+    });
+  };
+
+  // Secure Monkey-Patchery. Sweet, divine monkey-patchery...
+  var _wrap = function(wrappedFunction)
+  {
+    _activate();
+    wrappedFunction();
+    _deactivate();
+  };
+
+  return { registerPatch: _registerPatch
+         , activate:      _activate
+         , deactivate:    _deactivate
+         , wrap:          _wrap
+         };
 };
